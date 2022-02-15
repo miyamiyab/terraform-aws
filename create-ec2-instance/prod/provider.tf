08e3492096d15a4552dca5771702a08c017b90fc
Masanori Miyashita <walnutflower339@gmail.com>
2022-02-15 22:21:46 +0900

    Revert "Revert "Develop""

--- create-ec2-instance/prod/provider.tf
+++ create-ec2-instance/prod/provider.tf
@@ -2,18 +2,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.00"
+      version = "~> 3.27"
     }
   }
-
   backend "s3" {
     bucket  = "miyashita-tfstate"
     region  = "ap-northeast-1"
     encrypt = true
   }
-  required_version = ">= 0.15.0"
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-  region = "ap-northeast-1"
+  profile = "shino_admin"
+  region  = "ap-northeast-1"
 }
